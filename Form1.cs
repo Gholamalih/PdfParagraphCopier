4cb8056a40248155a952d9b85fc06b58cf97d7d4
Gholamalih <Gholamali.hosseini.behjani@gmail.com>
2023-02-27 15:21:05 +0330

    bug-fix: Catch exceptions

--- Form1.cs
+++ Form1.cs
@@ -16,16 +16,22 @@ namespace GTrans
         {
             Destination_TextBox.Text = Source_TextBox.Text.Replace("\r\n", " ");
 
-            string nullTerminatedStr = Destination_TextBox.Text + "\0";
-            byte[] strBytes = Encoding.Unicode.GetBytes(nullTerminatedStr);
-            IntPtr hglobal = Marshal.AllocHGlobal(strBytes.Length);
-            Marshal.Copy(strBytes, 0, hglobal, strBytes.Length);
-            Program.OpenClipboard(IntPtr.Zero);
-            Program.EmptyClipboard();
-            Program.SetClipboardData(13, hglobal);
-            Program.CloseClipboard();
-            Marshal.FreeHGlobal(hglobal);
-
+            try
+            {
+                string nullTerminatedStr = Destination_TextBox.Text + "\0";
+                byte[] strBytes = Encoding.Unicode.GetBytes(nullTerminatedStr);
+                IntPtr hglobal = Marshal.AllocHGlobal(strBytes.Length);
+                Marshal.Copy(strBytes, 0, hglobal, strBytes.Length);
+                Program.OpenClipboard(IntPtr.Zero);
+                Program.EmptyClipboard();
+                Program.SetClipboardData(13, hglobal);
+                Program.CloseClipboard();
+                Marshal.FreeHGlobal(hglobal);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void sharpClipboard1_ClipboardChanged(object sender, WK.Libraries.SharpClipboardNS.SharpClipboard.ClipboardChangedEventArgs e)
